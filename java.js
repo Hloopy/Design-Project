a9abee6fd99e3c7c981748aa0b2295772851a1eb
Hloopy <rfortin03@lbpearson.ca>
2025-01-10 15:29:29 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,37 @@
+onebutton.addEventListener('click',function(){
+    document.getElementById('onecontainer').style.backgroundColor = 'red';
+})
+twobutton.addEventListener('click',function(){
+    document.getElementById('twocontainer').style.backgroundColor = 'red';
+})
+
+threebutton.addEventListener('click',function(){
+    document.getElementById('threecontainer').style.backgroundColor = 'red';
+})
+
+fourbutton.addEventListener('click',function(){
+    document.getElementById('fourcontainer').style.backgroundColor = 'red';
+})
+
+fivebutton.addEventListener('click',function(){
+    document.getElementById('fivecontainer').style.backgroundColor = 'red';
+})
+
+sixbutton.addEventListener('click',function(){
+    document.getElementById('sixcontainer').style.backgroundColor = 'red';
+})
+
+sevenbutton.addEventListener('click',function(){
+    document.getElementById('sevencontainer').style.backgroundColor = 'red';
+})
+
+eightbutton.addEventListener('click',function(){
+    document.getElementById('eightcontainer').style.backgroundColor = 'red';
+})
+
+ninebutton.addEventListener('click',function(){
+    document.getElementById('ninecontainer').style.backgroundColor = 'red';
+})
+
+
+//I'm sure this can be improved, but it works, so 'if it aint broke, don't fix it' //
